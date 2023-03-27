ac6ef904e93b3d9b2a4ef25778d541677b670dae
Kamchybek Alzhanbaev <aljanbaevkam@gmail.com>
2023-03-27 17:09:57 +0600

    Task1 is done

---
+++ src/d1/Task1.java
@@ -0,0 +1,15 @@
+package d1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(i + "--JAVA ");
+            i++;
+        }
+        System.out.println();
+        for (i = 1; i <= 10; i++) {
+            System.out.print(i + "--JAVA ");
+        }
+    }
+}
